339f866fbce04d01e8ae46e4cefbc07c122b2d63
Houzar <po7dwy@net.plm.eds.com>
2017-03-06 18:21:18 +0100

    Sample479b.java

---
+++ Sample479b.java
@@ -0,0 +1,34 @@
+
+package com.siemens.polarion.pack;
+
+/**
+  * Defined formats:
+  * Full @wi.implements test/RT-479:1000 message
+  * No revision @wi.implements test/RT-479 message
+  * And no project @wi.implements RT-479 message
+  * And no role @wi RT-479 message
+  * Full with url @wi.implements http://polariondomain.com/polarion/#/project/test/workitem?id=RT-479&revision=5678 message
+  * With HTTPS protocol @wi.implements https://polariondomain.com/polarion/#/project/test/workitem?id=RT-479&revision=5678 message
+  * No revision @wi.implements http://polariondomain.com/polarion/#/project/test/workitem?id=RT-479 message
+  * Multiline {@wi.implements test/RT-479:1000 message  
+  * Continue
+  * Continue further}
+  * Another content
+  */
+public class Sample {
+
+   /**
+    * Mixed style links, complex pasing examples
+    * Linked to @wi test/RT-479
+    * Linked to {@wi test/RT-479} and {@wi test/RT-479:100}
+    * Linked to {@wi test/RT-479 some message
+    * Continue
+    * Continue further} and {@wi test/RT-479}
+    * Linked to {@wi test/RT-479} and @wi test/RT-479 last message
+    * Another content
+    */
+    public FixtureSample()
+    {
+
+    }
+}
